59f744b121f3a1ee9383183ffbf75a01a5d1cbea
Mixbra <misael110405@gmail.com>
2025-05-29 08:34:18 -0500

    Add files via upload

---
+++ ED1_29.05/ejercicios/FrmArbolBinarioBusqueda.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace ejercicios
+{
+    partial class FrmArbolBinarioBusqueda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.btnInsertarRaiz = new System.Windows.Forms.Button();
+            this.textNumero = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PreOrden = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.InOrden = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PostOrden = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Buscarbtn = new System.Windows.Forms.Button();
+            this.btnRecorrer = new System.Windows.Forms.Button();
+            this.Eliminarbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(226, 125);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(151, 217);
+            this.treeView1.TabIndex = 58;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // btnInsertarRaiz
+            // 
+            this.btnInsertarRaiz.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsertarRaiz.Location = new System.Drawing.Point(88, 125);
+            this.btnInsertarRaiz.Name = "btnInsertarRaiz";
+            this.btnInsertarRaiz.Size = new System.Drawing.Size(118, 35);
+            this.btnInsertarRaiz.TabIndex = 52;
+            this.btnInsertarRaiz.Text = "Insertar Raíz";
+            this.btnInsertarRaiz.UseVisualStyleBackColor = true;
+            this.btnInsertarRaiz.Click += new System.EventHandler(this.btnInsertarRaiz_Click);
+            // 
+            // textNumero
+            // 
+            this.textNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNumero.Location = new System.Drawing.Point(226, 92);
+            this.textNumero.Name = "textNumero";
+            this.textNumero.Size = new System.Drawing.Size(151, 26);
+            this.textNumero.TabIndex = 50;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(91, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 20);
+            this.label2.TabIndex = 49;
+            this.label2.Text = "Ingrese número: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 25);
+            this.label1.TabIndex = 48;
+            this.label1.Text = "ARBOL BINARIO BUSQUEDA";
+            // 
+            // PreOrden
+            // 
+            this.PreOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PreOrden.Location = new System.Drawing.Point(226, 362);
+            this.PreOrden.Name = "PreOrden";
+            this.PreOrden.Size = new System.Drawing.Size(151, 26);
+            this.PreOrden.TabIndex = 60;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(110, 362);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 59;
+            this.label3.Text = "PreOrden:";
+            // 
+            // InOrden
+            // 
+            this.InOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InOrden.Location = new System.Drawing.Point(226, 407);
+            this.InOrden.Name = "InOrden";
+            this.InOrden.Size = new System.Drawing.Size(151, 26);
+            this.InOrden.TabIndex = 62;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(110, 410);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 20);
+            this.label4.TabIndex = 61;
+            this.label4.Text = "InOrden:";
+            this.label4.Click += new System.EventHandler(this.btnRecorrer_Click);
+            // 
+            // PostOrden
+            // 
+            this.PostOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PostOrden.Location = new System.Drawing.Point(226, 455);
+            this.PostOrden.Name = "PostOrden";
+            this.PostOrden.Size = new System.Drawing.Size(151, 26);
+            this.PostOrden.TabIndex = 64;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(110, 458);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 20);
+            this.label5.TabIndex = 63;
+            this.label5.Text = "PostOrden:";
+            // 
+            // Buscarbtn
+            // 
+            this.Buscarbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Buscarbtn.Location = new System.Drawing.Point(88, 176);
+            this.Buscarbtn.Name = "Buscarbtn";
+            this.Buscarbtn.Size = new System.Drawing.Size(118, 35);
+            this.Buscarbtn.TabIndex = 65;
+            this.Buscarbtn.Text = "Buscar";
+            this.Buscarbtn.UseVisualStyleBackColor = true;
+            this.Buscarbtn.Click += new System.EventHandler(this.Buscarbtn_Click);
+            // 
+            // btnRecorrer
+            // 
+            this.btnRecorrer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecorrer.Location = new System.Drawing.Point(88, 308);
+            this.btnRecorrer.Name = "btnRecorrer";
+            this.btnRecorrer.Size = new System.Drawing.Size(118, 34);
+            this.btnRecorrer.TabIndex = 66;
+            this.btnRecorrer.Text = "Recorrido";
+            this.btnRecorrer.UseVisualStyleBackColor = true;
+            this.btnRecorrer.Click += new System.EventHandler(this.btnRecorrer_Click);
+            // 
+            // Eliminarbtn
+            // 
+            this.Eliminarbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Eliminarbtn.Location = new System.Drawing.Point(88, 226);
+            this.Eliminarbtn.Name = "Eliminarbtn";
+            this.Eliminarbtn.Size = new System.Drawing.Size(118, 35);
+            this.Eliminarbtn.TabIndex = 67;
+            this.Eliminarbtn.Text = "Eliminar";
+            this.Eliminarbtn.UseVisualStyleBackColor = true;
+            this.Eliminarbtn.Click += new System.EventHandler(this.Eliminarbtn_Click);
+            // 
+            // FrmArbolBinarioBusqueda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 525);
+            this.Controls.Add(this.Eliminarbtn);
+            this.Controls.Add(this.btnRecorrer);
+            this.Controls.Add(this.Buscarbtn);
+            this.Controls.Add(this.PostOrden);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.InOrden);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.PreOrden);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnInsertarRaiz);
+            this.Controls.Add(this.textNumero);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmArbolBinarioBusqueda";
+            this.Text = "FrmArbolBinarioBusqueda";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button btnInsertarRaiz;
+        private System.Windows.Forms.TextBox textNumero;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PreOrden;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox InOrden;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox PostOrden;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Buscarbtn;
+        private System.Windows.Forms.Button btnRecorrer;
+        private System.Windows.Forms.Button Eliminarbtn;
+    }
+}
---
+++ ED1_29.05/ejercicios/clases/ArbolBinario.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Hosting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicios.clases
+{
+    class ArbolBinario
+    {
+        public NodoAB insertar(NodoAB actual, int num)
+        {
+            NodoAB nuevo = new NodoAB(num);
+
+            if (actual == null)
+            {
+                return nuevo;
+            }
+            else if ((actual.izquierdo != null || actual.derecho == null) &&
+                    (actual.izquierdo == null || actual.derecho != null))
+            {
+                MessageBox.Show("Nodo ocupado");
+            }
+            else if (actual.izquierdo == null) { actual.izquierdo = nuevo; }
+            else actual.derecho = nuevo;
+            { 
+            return actual;
+            }
+
+        }
+        public void mostrar(NodoAB actual, TreeView tree, TreeNode padre)
+        {
+            if (actual == null) return;
+            TreeNode nuevo = new TreeNode(actual.numero.ToString());
+            nuevo.Tag = actual;
+            if (padre == null)
+            {
+                tree.Nodes.Add(nuevo);
+            }
+            else
+            {
+                padre.Nodes.Add(nuevo);
+            }
+            if (actual.izquierdo != null)
+            {
+                mostrar(actual.izquierdo, tree, nuevo);
+            }
+            if (actual.derecho != null)
+            {
+                mostrar(actual.derecho, tree, nuevo);
+            }
+        }
+        public int altura(NodoAB actual) { 
+        if(actual == null) return 0;
+            int altIzquierda = altura(actual.izquierdo);
+            int altDereha = altura(actual.derecho);
+          return Math.Max(altIzquierda, altDereha) + 1;
+
+        }
+
+
+    }
+}
